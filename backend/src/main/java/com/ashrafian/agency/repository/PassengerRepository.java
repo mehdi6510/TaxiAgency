65ee078a9f9fdc0d39803db87fa0ce46f60e4139
zahra-ashrafian <@shrafian01>
2023-03-02 18:27:54 +0330

    add passenger repository method

--- backend/src/main/java/com/ashrafian/agency/repository/PassengerRepository.java
+++ backend/src/main/java/com/ashrafian/agency/repository/PassengerRepository.java
@@ -2,9 +2,19 @@ package com.ashrafian.agency.repository;
 
 import com.ashrafian.agency.model.entity.Passenger;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface PassengerRepository extends JpaRepository<Passenger, Long> {
+    String GET_PASSENGER_BY_NAME_AND_ID = "GET_PASSENGER_BY_NAME_AND_ID";
+    String PASSENGER = " passenger";
+    @Query(name = GET_PASSENGER_BY_NAME_AND_ID,
+            value = "select   * from " + PASSENGER + "  where firstName=:firstName and lastName=:lastName ", nativeQuery = true)
+    List<Passenger> getPassengerByNameAndLastName(@Param("firstName") String firstName,@Param("lastName") String lastName);
+
 
 }
